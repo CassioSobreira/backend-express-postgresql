333b9b7302f23d02401ebf193cf832c329d2bcac
CassioGuimaraes <131621517+CassioSobreira@users.noreply.github.com>
2025-10-16 16:59:10 -0300

    add conexão com o banco

---
+++ database/configdb.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    mongoose.set('strictQuery', true);
+    await mongoose.connect(process.env.MONGO_URI, { dbName: process.env.MONGO_DB_NAME });
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
+
+export default { connectDB };
