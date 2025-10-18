0c3823bd84d558c55fe9e1f7ddcb9e0f0ed972d5
CassioGuimaraes <131621517+CassioSobreira@users.noreply.github.com>
2025-10-18 18:20:46 -0300

    .

--- database/configdb.js
+++ database/configdb.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     mongoose.set('strictQuery', true);
     await mongoose.connect(process.env.MONGO_URI, { dbName: process.env.MONGO_DB_NAME });
-    console.log('MongoDB connected successfully');
+    console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
     process.exit(1);
