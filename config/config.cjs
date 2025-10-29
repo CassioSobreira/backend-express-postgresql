fa72dba1bcde6e4c14d7072cf1f6f0709c1d2841
CassioGuimaraes <131621517+CassioSobreira@users.noreply.github.com>
2025-10-29 15:52:23 -0300

    Alteração no Database

--- config/config.cjs
+++ config/config.cjs
@@ -15,7 +15,7 @@ module.exports = {
   development: {
     username: process.env.POSTGRES_USER,    
     password: process.env.POSTGRES_PASSWORD, 
-    database: process.env.POSTGRES_DB,      
+    database: process.env.POSTGRES_DATABASE,      
     host: process.env.POSTGRES_HOST || 'localhost', 
     port: parseInt(process.env.POSTGRES_PORT || '5432', 10), 
     dialect: 'postgres'
