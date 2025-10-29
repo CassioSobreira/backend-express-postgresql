da7800d291bc649981cd8f69da7a41b06f99c2e3
CassioGuimaraes <131621517+CassioSobreira@users.noreply.github.com>
2025-10-29 17:00:31 -0300

    Adição pg

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,28 +1,30 @@
+import pg from 'pg'; 
 import { Sequelize } from 'sequelize';
 import { sequelize } from '../database'; 
 import { initUser } from './User';
 import { initMovie } from './Movie';
+
 
 const User = initUser(sequelize);
 const Movie = initMovie(sequelize);
 
 
 const db = {
-  sequelize, 
-  Sequelize, 
-  User,      
-  Movie,     
+  sequelize,
+  Sequelize,
+  User,
+  Movie,
 };
 
 
 Object.values(db).forEach((model: any) => {
   if (model.associate) {
-    console.log(`>>> [MODELS] Configurando associações para ${model.name}`);
     model.associate(db);
   }
 });
 
-console.log('>>> [MODELS] Modelos Sequelize inicializados e associados.');
 
 export default db;
+
+
 
